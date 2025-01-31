988aaf3ce1619bd5e9bf9f9e773bd61aee3931eb
egarrisxn <egarrisxn@gmail.com>
2025-01-31 17:57:14 -0500

    og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,6 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
-  manifest: "/manifest.json",
   openGraph: {
     title: "DevBuild Resume!",
     description: "A web app that helps you build a resume in minutes.",
@@ -59,22 +58,6 @@ export const metadata: Metadata = {
         width: 1200,
         height: 630,
         alt: "A web app that helps you build a resume in minutes.",
-      },
-    ],
-  },
-  icons: {
-    icon: [
-      { url: "/favicon.ico", sizes: "48x48", type: "image/x-icon" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-    ],
-    apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
-    ],
-    other: [
-      {
-        rel: "apple-touch-icon-precomposed",
-        url: "/apple-touch-icon-precomposed.png",
       },
     ],
   },
